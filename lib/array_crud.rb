75fdd5abf112ce453091ca8f65e81fc25e36f0f2
Michael Schneider <mic.sch91@gmail.com>
2020-03-11 15:53:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,6 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  wow_array = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
